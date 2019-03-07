3f4d16ff20aedb5938679e655243e1572ec19023
DADYSoukaina <dadysoukaina@gmail.com>
2019-03-07 12:38:39 +0100

    Welcome to the world of the String class

--- src/chapter4/StringClass.java
+++ src/chapter4/StringClass.java
@@ -4,5 +4,7 @@ public class StringClass {
 
 	public static void mainn(String[]args) {
         System.out.println( "First step: I only still have 12 days to finish the OCA exam preparation, you can do it Souka ;) InxaAllah :) " );
+       //xfgdghjd. 
+	
 	}
 }
